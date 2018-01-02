4c4c705f8722151cc2851a6c8299a5f7531fb242
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-01-02 11:40:18 -0200

    Support powerful access

---
+++ test/fixtures/function-call/actual.js
@@ -0,0 +1,3 @@
+const assocToUsers = assoc(_, _, users)
+const containedInList = contains(_, list)
+const preserved = f(1)
---
+++ test/fixtures/function-call/expected.js
@@ -0,0 +1,3 @@
+const assocToUsers = (_2, _3) => assoc(_2, _3, users);
+const containedInList = _4 => contains(_4, list);
+const preserved = f(1);
