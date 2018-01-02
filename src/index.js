2033451b309ce4ea2a133f7cd08ddd646f2042b7
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-01-02 13:49:21 -0200

    Curry support

--- src/index.js
+++ src/index.js
@@ -2,159 +2,164 @@ const hasNoHoles = path =>
     !!path.findParent(({ node }) =>
         node.directives && node.directives.some(({ value }) => value.value === 'no holes'))
 
-export default ({ types: t }) => ({
-    visitor: {
-        CallExpression(path) {
-            if (hasNoHoles(path)) {
-                return
-            }
+const currier = (curry, t) => node =>
+    curry
+        ? t.callExpression(t.identifier(curry), node)
+        : node
 
-            const isUnderscore = node => t.isIdentifier(node, { name: '_' })
-            const parameters = []
+export default ({ types: t }, options = {}) => {
+    const curried = currier(options.curry, t)
+    const isUnderscore = node => t.isIdentifier(node, { name: '_' })
 
-            if (t.isMemberExpression(path.node.callee) && isUnderscore(path.node.callee.object)
-                || isUnderscore(path.node.callee)) {
-                parameters.push(path.scope.generateUidIdentifier('_'))
-            }
+    return {
+        visitor: {
+            CallExpression(path) {
+                if (hasNoHoles(path)) {
+                    return
+                }
 
-            path.node.arguments
-                .filter(isUnderscore)
-                .forEach(arg => {
+                const parameters = []
+                if (t.isMemberExpression(path.node.callee) && isUnderscore(path.node.callee.object)
+                    || isUnderscore(path.node.callee)) {
                     parameters.push(path.scope.generateUidIdentifier('_'))
-                })
+                }
 
-            if (parameters.length === 0) {
-                return
-            }
+                path.node.arguments
+                    .filter(isUnderscore)
+                    .forEach(arg => {
+                        parameters.push(path.scope.generateUidIdentifier('_'))
+                    })
 
-            const provider = parameters.slice()
-            const transformCallee = callee =>
-                isUnderscore(callee)
-                    ? provider.shift()
-                    : t.isMemberExpression(callee) && isUnderscore(callee.object)
-                        ? t.memberExpression(provider.shift(), callee.property, callee.computed)
-                        : callee
+                if (parameters.length === 0) {
+                    return
+                }
 
-            const lambda = t.arrowFunctionExpression(
-                parameters,
-                t.callExpression(
-                    transformCallee(path.node.callee),
-                    path.node.arguments.map(arg =>
-                        isUnderscore(arg)
-                            ? provider.shift()
-                            : arg)
+                const provider = parameters.slice()
+                const transformCallee = callee =>
+                    isUnderscore(callee)
+                        ? provider.shift()
+                        : t.isMemberExpression(callee) && isUnderscore(callee.object)
+                            ? t.memberExpression(provider.shift(), callee.property, callee.computed)
+                            : callee
+
+                const lambda = t.arrowFunctionExpression(
+                    parameters,
+                    t.callExpression(
+                        transformCallee(path.node.callee),
+                        path.node.arguments.map(arg =>
+                            isUnderscore(arg)
+                                ? provider.shift()
+                                : arg)
+                    )
                 )
-            )
 
-            path.replaceWith(lambda)
-        },
+                path.replaceWith(curried(lambda))
+            },
 
-        MemberExpression(path) {
-            if (hasNoHoles(path)) {
-                return
-            }
+            MemberExpression(path) {
+                if (hasNoHoles(path)) {
+                    return
+                }
 
-            const isUnderscore = node => t.isIdentifier(node, { name: '_' })
-            const holes = []
+                const holes = []
 
+                if (isUnderscore(path.node.object)) {
+                    holes.push(path.scope.generateUidIdentifier('_'))
+                }
 
-            if (isUnderscore(path.node.object)) {
-                holes.push(path.scope.generateUidIdentifier('_'))
-            }
+                if (isUnderscore(path.node.property)) {
+                    holes.push(path.scope.generateUidIdentifier('_'))
+                }
 
-            if (isUnderscore(path.node.property)) {
-                holes.push(path.scope.generateUidIdentifier('_'))
-            }
+                if (holes.length === 0) {
+                    return
+                }
 
-            if (holes.length === 0) {
-                return
-            }
-
-            const provider = holes.slice()
-            const transform = node => isUnderscore(node)
-                ? provider.shift()
-                : node
-
-            const lambda = t.arrowFunctionExpression(
-                holes,
-                t.memberExpression(
-                    transform(path.node.object),
-                    transform(path.node.property),
-                    path.node.computed
-                )
-            )
-
-            path.replaceWith(lambda)
-        },
-
-        BinaryExpression(path) {
-            if (hasNoHoles(path)) {
-                return
-            }
-
-            const parameters = []
-            const isUnderscore = node => t.isIdentifier(node, { name: '_' })
-
-            if (isUnderscore(path.node.left)) {
-                parameters.push(path.scope.generateUidIdentifier('_'))
-            }
-
-            if (isUnderscore(path.node.right)) {
-                parameters.push(path.scope.generateUidIdentifier('_'))
-            }
-
-            if (parameters.length === 0) {
-                return
-            }
-
-            const provider = parameters.slice()
-            const transform = node =>
-                isUnderscore(node)
+                const provider = holes.slice()
+                const transform = node => isUnderscore(node)
                     ? provider.shift()
                     : node
 
-            const lambda = t.arrowFunctionExpression(
-                parameters,
-                t.BinaryExpression(
-                    path.node.operator,
-                    transform(path.node.left),
-                    transform(path.node.right)
+                const lambda = t.arrowFunctionExpression(
+                    holes,
+                    t.memberExpression(
+                        transform(path.node.object),
+                        transform(path.node.property),
+                        path.node.computed
+                    )
                 )
-            )
 
-            path.replaceWith(lambda)
-        },
+                path.replaceWith(curried(lambda))
+            },
 
-        UnaryExpression(path) {
-            if (!t.isIdentifier(path.node.argument, { name: '_' }) || hasNoHoles(path)) {
-                return
-            }
+            BinaryExpression(path) {
+                if (hasNoHoles(path)) {
+                    return
+                }
 
-            const parameter = path.scope.generateUidIdentifier('_')
-            const lambda = t.arrowFunctionExpression(
-                [parameter],
-                t.unaryExpression(
-                    path.node.operator,
-                    parameter,
-                    path.node.prefix
+                const parameters = []
+
+                if (isUnderscore(path.node.left)) {
+                    parameters.push(path.scope.generateUidIdentifier('_'))
+                }
+
+                if (isUnderscore(path.node.right)) {
+                    parameters.push(path.scope.generateUidIdentifier('_'))
+                }
+
+                if (parameters.length === 0) {
+                    return
+                }
+
+                const provider = parameters.slice()
+                const transform = node =>
+                    isUnderscore(node)
+                        ? provider.shift()
+                        : node
+
+                const lambda = t.arrowFunctionExpression(
+                    parameters,
+                    t.BinaryExpression(
+                        path.node.operator,
+                        transform(path.node.left),
+                        transform(path.node.right)
+                    )
                 )
-            )
 
-            path.replaceWith(lambda)
-        },
+                path.replaceWith(curried(lambda))
+            },
 
-        Identifier(path) {
-            if (path.node.name !== '_' || hasNoHoles(path)) {
-                return
+            UnaryExpression(path) {
+                if (!t.isIdentifier(path.node.argument, { name: '_' }) || hasNoHoles(path)) {
+                    return
+                }
+
+                const parameter = path.scope.generateUidIdentifier('_')
+                const lambda = t.arrowFunctionExpression(
+                    [parameter],
+                    t.unaryExpression(
+                        path.node.operator,
+                        parameter,
+                        path.node.prefix
+                    )
+                )
+
+                path.replaceWith(curried(lambda))
+            },
+
+            Identifier(path) {
+                if (path.node.name !== '_' || hasNoHoles(path)) {
+                    return
+                }
+
+                const parameter = path.scope.generateUidIdentifier('_')
+                const lambda = t.arrowFunctionExpression(
+                    [parameter],
+                    parameter
+                )
+
+                path.replaceWith(curried(lambda))
             }
-
-            const parameter = path.scope.generateUidIdentifier('_')
-            const lambda = t.arrowFunctionExpression(
-                [parameter],
-                parameter
-            )
-
-            path.replaceWith(lambda)
         }
     }
-})
+}
