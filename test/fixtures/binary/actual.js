47fc663a871510b7e7fef520d60cfae950323ad8
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-01-02 10:49:07 -0200

    Binary expression is working

---
+++ test/fixtures/binary/actual.js
@@ -0,0 +1,5 @@
+const add = _ + _
+const inc = _ + 1
+const dec = _ - 1
+const subFrom100 = 100 - _
+const two = 1 + 1
---
+++ test/fixtures/binary/expected.js
@@ -0,0 +1,5 @@
+const add = (_2, _3) => _2 + _3;
+const inc = _4 => _4 + 1;
+const dec = _5 => _5 - 1;
+const subFrom100 = _6 => 100 - _6;
+const two = 1 + 1;
